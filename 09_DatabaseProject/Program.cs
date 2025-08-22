2e20d33e9b297481f3439c6d44dc4271f6cc067e
UMUT <umutby.ldg@gmail.com>
2025-08-22 18:10:34 +0300

    SQLConnection

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,40 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
+            //Ado.net
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Katagori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1-Katgoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-------------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=UMUTBOYLUDAG\\SQLEXPRESS;initial Catalog=EgitimKampiDb; integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCatagory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
 
 
             Console.Read();
